e103038d3384b1c6db61d54cc1eca81b556aa08b
Jared Williams <accounts@jaredwilliams.com.au>
2019-07-21 13:40:04 +0800

    Allow anyone

--- reactapp/src/containers/app/index.js
+++ reactapp/src/containers/app/index.js
@@ -30,146 +30,132 @@ const RouteWithMeta = ({ meta, ...routeProps }) => {
   return <Route {...routeProps} />
 }
 
-const App = ({ searchTerm }) => {
-  if (
-    window.location.href.includes('mtgcardrank') &&
-    localStorage.getItem('sparky') !== 'yes'
-  ) {
-    return (
-      <div>
-        Coming soon
-        <span onClick={() => localStorage.setItem('sparky', 'yes')}>...</span>
-      </div>
-    )
-  }
+const App = ({ searchTerm }) => (
+  <>
+    <PageHeader />
+    <main className="main">
+      <Container maxWidth="lg">
+        <SearchResults />
 
-  return (
-    <>
-      <PageHeader />
-      <main className="main">
-        <Container maxWidth="lg">
-          <SearchResults />
-
-          {!searchTerm && (
-            <Switch>
-              <RouteWithMeta
-                exact
-                path={routes.home}
-                component={Home}
-                meta={{
-                  title: 'Rank different Magic The Gathering cards with lists',
-                  description:
-                    'A website that allows anyone to rank Magic The Gathering cards by any metric they want. Other users can commit and rate your lists to determine the best list of cards.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.login}
-                component={Login}
-                meta={{
-                  title: 'Log in to manage your lists, vote and comment',
-                  description:
-                    'Use the log in form to log in to your account so that you can create, edit, comment on and vote on lists of cards.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.logout}
-                component={Logout}
-                meta={{
-                  title: 'Logging you out',
-                  description:
-                    'Visit this page to automatically log out of your account.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.lists}
-                component={Lists}
-                meta={{
-                  title: 'Browse the lists of Magic The Gathering cards',
-                  description:
-                    'All available lists of Magic The Gathering cards are shown here so that you can browse them.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.card}
-                component={Card}
-                meta={{
-                  title: 'View a single card',
-                  description:
-                    'An overview of a single Magic The Gathering card that shows statistics such as which lists the card is featured in.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.createList}
-                component={CreateList}
-                meta={{
-                  title: 'Create a new list',
-                  description:
-                    'A form that lets you create a new list of Magic The Gathering cards.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.viewListWithVar}
-                component={ViewList}
-                // TODO: Use list title as page title
-                meta={{
-                  title: 'View a list of Magic The Gathering cards',
-                  description:
-                    'This is a list of Magic The Gathering cards, who created the list and by which metric they used to create the list.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.editListWithVar}
-                component={EditList}
-                meta={{
-                  title: 'Edit your list',
-                  description:
-                    'This form lets you edit your list of Magic The Gathering cards.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.admin}
-                component={Admin}
-                meta={{
-                  title: 'Admins only area',
-                  description: 'A restricted space for admins only.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.myAccount}
-                component={MyAccount}
-                meta={{
-                  title: 'View details about your account',
-                  description:
-                    'An overview of your account including a way to change your username and see statistics of your account.'
-                }}
-              />
-              <RouteWithMeta
-                exact
-                path={routes.privacyPolicy}
-                component={PrivacyPolicy}
-                meta={{
-                  title: 'Our privacy policy',
-                  description:
-                    'View the privacy policy of our website including what we do with your personal data.'
-                }}
-              />
-            </Switch>
-          )}
-        </Container>
-      </main>
-      <PageFooter />
-    </>
-  )
-}
+        {!searchTerm && (
+          <Switch>
+            <RouteWithMeta
+              exact
+              path={routes.home}
+              component={Home}
+              meta={{
+                title: 'Rank different Magic The Gathering cards with lists',
+                description:
+                  'A website that allows anyone to rank Magic The Gathering cards by any metric they want. Other users can commit and rate your lists to determine the best list of cards.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.login}
+              component={Login}
+              meta={{
+                title: 'Log in to manage your lists, vote and comment',
+                description:
+                  'Use the log in form to log in to your account so that you can create, edit, comment on and vote on lists of cards.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.logout}
+              component={Logout}
+              meta={{
+                title: 'Logging you out',
+                description:
+                  'Visit this page to automatically log out of your account.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.lists}
+              component={Lists}
+              meta={{
+                title: 'Browse the lists of Magic The Gathering cards',
+                description:
+                  'All available lists of Magic The Gathering cards are shown here so that you can browse them.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.card}
+              component={Card}
+              meta={{
+                title: 'View a single card',
+                description:
+                  'An overview of a single Magic The Gathering card that shows statistics such as which lists the card is featured in.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.createList}
+              component={CreateList}
+              meta={{
+                title: 'Create a new list',
+                description:
+                  'A form that lets you create a new list of Magic The Gathering cards.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.viewListWithVar}
+              component={ViewList}
+              // TODO: Use list title as page title
+              meta={{
+                title: 'View a list of Magic The Gathering cards',
+                description:
+                  'This is a list of Magic The Gathering cards, who created the list and by which metric they used to create the list.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.editListWithVar}
+              component={EditList}
+              meta={{
+                title: 'Edit your list',
+                description:
+                  'This form lets you edit your list of Magic The Gathering cards.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.admin}
+              component={Admin}
+              meta={{
+                title: 'Admins only area',
+                description: 'A restricted space for admins only.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.myAccount}
+              component={MyAccount}
+              meta={{
+                title: 'View details about your account',
+                description:
+                  'An overview of your account including a way to change your username and see statistics of your account.'
+              }}
+            />
+            <RouteWithMeta
+              exact
+              path={routes.privacyPolicy}
+              component={PrivacyPolicy}
+              meta={{
+                title: 'Our privacy policy',
+                description:
+                  'View the privacy policy of our website including what we do with your personal data.'
+              }}
+            />
+          </Switch>
+        )}
+      </Container>
+    </main>
+    <PageFooter />
+  </>
+)
 
 const mapStateToProps = ({ app: { searchTerm } }) => ({ searchTerm })
 
