be994ea0fb9df5019271c9c83db36eb50e35eafb
Jared Williams <accounts@jaredwilliams.com.au>
2019-07-21 10:41:07 +0800

    Sentry.io

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -8,10 +8,15 @@ import ReactReduxFirebaseProvider from 'react-redux-firebase/lib/ReactReduxFireb
 import store, { history } from './store'
 import App from './containers/app'
 import { trackAction, actions } from './analytics'
+import * as Sentry from '@sentry/browser'
 
 import 'sanitize.css/sanitize.css'
 import './assets/css/theme.css'
 import './assets/css/mana.min.css'
+
+Sentry.init({
+  dsn: 'https://eefc3e7e553546a0bf725a90f3048ae9@sentry.io/1509721'
+})
 
 history.listen(location => {
   trackAction(actions.NAVIGATE, location)
